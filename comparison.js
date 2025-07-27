40cb04de5e8db5669b7c4d019b5413bcc924308a
sohag142 <sohag2879@gmail.com>
2025-07-27 21:34:52 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,11 @@
+// comparison
+console.log(5 > 10); // false
+console.log(5 < 10); // true
+console.log(5 >= 10); // false
+console.log(5 <= 10); // true
+
+
+var parayaraPrice = 100;
+var applePrice = 50;
+console.log(parayaraPrice > applePrice); // true
+console.log(parayaraPrice < applePrice); // false
