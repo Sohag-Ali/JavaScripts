ad9f132ecf29b251b09a0b47444ca7a5a9c62004
sohag142 <sohag2879@gmail.com>
2025-07-28 09:15:57 +0600

    M-14_Task-01

---
+++ IfElseM14-t1.js
@@ -0,0 +1,10 @@
+let burgerPrice = 510;
+
+if (burgerPrice > 500)
+{
+    console.log("You can take a free coke...");
+}
+else
+{
+    console.log("You can take coke but pay for it 30tk");
+}
--- comparison.js
+++ comparison.js
@@ -8,4 +8,4 @@ console.log(5 <= 10); // true
 var parayaraPrice = 100;
 var applePrice = 50;
 console.log(parayaraPrice > applePrice); // true
-console.log(parayaraPrice < applePrice); // false 
+console.log(parayaraPrice < applePrice); // false   
