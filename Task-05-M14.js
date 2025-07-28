4caf89bba63ba2d9e8b4b9258dde033ac40d4f87
sohag142 <sohag2879@gmail.com>
2025-07-28 10:58:39 +0600

    Task 05 m14

--- Task-05-M14.js
+++ Task-05-M14.js
@@ -26,9 +26,9 @@ console.log("Result (if-else):", result);
 
 
 
-let num1 = 10;
-let num2 = 5;
+// let num1 = 10;
+// let num2 = 5;
 
-let result = (num1 > num2) ? (num1 * 2) : (num1 + num2);
+// let result = (num1 > num2) ? (num1 * 2) : (num1 + num2);
 
-console.log("Result (ternary):", result);
+// console.log("Result (ternary):", result);
