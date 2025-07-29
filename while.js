c088874d41778f7fec673c137c528f9067b147bf
sohag142 <sohag2879@gmail.com>
2025-07-29 21:20:32 +0600

    While

---
+++ while.js
@@ -0,0 +1,12 @@
+const number = 7;
+if(number < 10) {
+    console.log("The number is less than 10.");
+} else {
+    console.log("The number is 10 or greater.");
+}
+
+let num = 0;
+while(num < 5) {
+    console.log("Current number is: " + num);
+    num++;
+}
