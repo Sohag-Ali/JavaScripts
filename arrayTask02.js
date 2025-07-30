dbc7392259735436635d34fa3b1da13a393b49a9
sohag142 <sohag2879@gmail.com>
2025-07-30 20:59:38 +0600

    array Task 02

---
+++ arrayTask02.js
@@ -0,0 +1,10 @@
+let touristSpots = ["Cox's Bazar", "Sundarbans", "Bandarban"];
+
+touristSpots.push("Saint Martin");
+console.log(touristSpots);
+
+touristSpots.push("Rangamati", "Sylhet");
+console.log(touristSpots);
+
+touristSpots.pop();
+console.log(touristSpots);
