a746779258b18d012519c891a9c6eee04f446667
sohag142 <sohag2879@gmail.com>
2025-07-30 21:06:15 +0600

    array Task 04

---
+++ arrayTask04.js
@@ -0,0 +1,17 @@
+
+let fruits = ["Apple", "Banana", "Mango"];
+let age = 25;
+let name = "Sohag";
+let isStudent = true;
+let scores = [90, 85, 88];
+let address = { city: "Dhaka", country: "Bangladesh" };
+
+
+console.log("age is an array:", Array.isArray(age));             
+console.log("fruits is an array:", Array.isArray(fruits));      
+console.log("name is an array:", Array.isArray(name));           
+console.log("isStudent is an array:", Array.isArray(isStudent)); 
+console.log("scores is an array:", Array.isArray(scores));
+console.log("address is an array:", Array.isArray(address));
+
+
