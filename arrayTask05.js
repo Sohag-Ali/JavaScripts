7b243cd93361512cc0af597142721881b5ada8f1
sohag142 <sohag2879@gmail.com>
2025-07-30 21:08:53 +0600

    array Task 05

---
+++ arrayTask05.js
@@ -0,0 +1,8 @@
+let fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];   
+let price = [10, 20, 30, 40, 50];
+
+console.log(fruits);
+console.log(price);
+
+let both = fruits.concat(price);
+console.log(both);
