31e728ca8e96e84e67506b25137c887ba56427f8
sohag142 <sohag2879@gmail.com>
2025-07-30 21:03:05 +0600

    array Task 03

---
+++ arrayTask03.js
@@ -0,0 +1,10 @@
+let books = ["Python", "javaScripts", "Java", "C++", "C#"]; 
+
+console.log(books);
+
+if (books.includes("javaScripts")) {
+    console.log("JavaScript is in the list.");
+}
+else {
+    console.log("JavaScript is not in the list.");
+}
