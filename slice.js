52a7eab12a4c04054c2999fa49d02c428a3c7169
sohag142 <sohag2879@gmail.com>
2025-07-31 20:57:21 +0600

    slice

---
+++ slice.js
@@ -0,0 +1,11 @@
+const address = "Sohag ali Rezwan sarkar";
+
+const part = address.slice(0,7);
+console.log (part);
+
+const sentence = " i am sohag , and i live in currently uttara, dhaka";
+console.log(sentence.split('a'));
+
+const friendss = "sohag,ali,rezwwan, afinee,wzhiha";
+const friends = friendss.split(',');
+console.log(friends);
