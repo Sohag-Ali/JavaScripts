fbc9c67ee81a9e7c22794ecc35cca32d24df8bb3
sohag142 <sohag2879@gmail.com>
2025-07-31 20:46:58 +0600

    string

---
+++ string.js
@@ -0,0 +1,25 @@
+const school = "Raj UK Uttara Model School";
+
+// console.log (school);
+
+const subject = 'chemistry';
+const book = "Chemistry";
+
+if (subject.toUpperCase() === book.toUpperCase()){
+    console.log("i am reading bok");
+}
+else
+{
+    console.log("i am no t reading in this book");
+}
+
+const drink = "water ";
+const liquid = " water";
+
+if (drink.trim() === liquid.trim())
+{
+    console.log ("drink water");
+}
+else {
+    console.log("ami water khaite pari nahh");
+}
