bd55e60c9a49e3c6e00212862b79a1ac1725172c
sohag142 <sohag2879@gmail.com>
2025-08-03 20:52:58 +0600

    assinment 04

---
+++ assingment-04.js
@@ -0,0 +1,15 @@
+var fileName = "slipdf.txt";
+
+if (fileName.slice(0, 1) === "#") {
+  console.log("Store");
+} 
+else if (fileName.slice(-4) === ".pdf") {
+  console.log("Store");
+} 
+else if (fileName.slice(-5) === ".docx") {
+  console.log("Store");
+} 
+else {
+  console.log("Delete");
+}
+
