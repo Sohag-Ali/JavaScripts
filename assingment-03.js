0b79b690264cae61150cfd6539befa0604a7bb9c
sohag142 <sohag2879@gmail.com>
2025-08-03 20:38:22 +0600

    assinment 03

--- assingment-03.js
+++ assingment-03.js
@@ -1,8 +1,11 @@
-var lastDay = 6;
-for (var i = 1; i <= lastDay; day++) {
-  if (day % 3 === 0) {
+
+// problem 03 solution...........
+var lastDay = 4;
+for (var i = 1; i <= lastDay; i++) {
+  if (i % 3 === 0) {
     console.log(i + " - medicine");
-  } else {
+  }
+  else {
     console.log(i + " - rest");
   }
 }
