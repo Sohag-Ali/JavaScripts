3043c30967c5ad6c434238b1bcc7ec2a54b86394
sohag142 <sohag2879@gmail.com>
2025-08-06 20:07:34 +0600

    Array Loop Task 1 M 18

---
+++ ArrayLoopTask/Task_01.js
@@ -0,0 +1,10 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// console.log(colors.reverse());
+// for ( let i = colors.length -1 ; i >= 0; i--){
+// console.log(colors[i]);
+// }
+const a=[];
+for ( let i = colors.length -1 ; i >= 0; i--){
+a.push(colors[i]);
+}
+console.log(a);
