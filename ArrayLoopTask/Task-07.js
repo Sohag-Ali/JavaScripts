f399a62f767d5575b692b967eac48ee0c7d1ff10
sohag142 <sohag2879@gmail.com>
2025-08-06 20:57:19 +0600

    Array Loop Task 7 M 18

---
+++ ArrayLoopTask/Task-07.js
@@ -0,0 +1,12 @@
+const array = [
+  [1, 2],
+  [3, 4],
+  [5, 6]
+];
+
+console.log ("Orginal array " + ":" );
+console.log(array);
+
+array[1][0] = 99;
+console.log ("Change array " + ":" );
+console.log(array);
