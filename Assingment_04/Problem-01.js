ee82b7ee85ab0ed6ff92108f5a841155dee8fbb3
sohag142 <sohag2879@gmail.com>
2025-08-13 18:36:12 +0600

    Assingment-04  Problem 01

---
+++ Assingment_04/Problem-01.js
@@ -0,0 +1,11 @@
+function totalFine(fare) {
+    if (typeof fare !== 'number' || fare <= 0) {
+        return "Invalid";
+    }
+    
+    let surcharge = fare * 0.20;
+    let extraCharge = 30;
+    let total = fare + surcharge + extraCharge;
+
+    return total;
+}
