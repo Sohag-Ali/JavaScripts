81c8d3519c0d9b5d74ee1d2dab9d33add87a6dd7
sohag142 <sohag2879@gmail.com>
2025-09-04 18:31:56 +0600

    es6

---
+++ M_29/defualt_parmeter.js
@@ -0,0 +1,13 @@
+function sum(num1=0,num2=0){
+    const total = num1 + num2;
+    console.log(total);
+}
+
+sum(20,30);
+sum(20);
+
+function multiply ( num1, num2){
+const result = num1 * num2 ;
+console.log(result);
+}
+multiply(2,3);
