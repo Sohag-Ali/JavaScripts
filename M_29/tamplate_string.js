9d297032877974a615d3ab6897f782b8e5eb60c9
sohag142 <sohag2879@gmail.com>
2025-09-05 08:52:02 +0600

    es6

---
+++ M_29/tamplate_string.js
@@ -0,0 +1,16 @@
+const poem = `amm pat jora jora
+marbe chabuk chorbe ghor
+ore khokha shore dara`
+
+console.log(poem);
+
+const arr = [20,65,95,75,24,26];
+console.log(arr);
+console.log(...arr);
+
+const max = Math.max(20,65,48,95,78);
+console.log(max);
+
+const device = {name:"suziki gixxer", brand:"suziki"}
+const{name} = device;
+console.log(name);
